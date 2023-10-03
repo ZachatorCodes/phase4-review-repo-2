4a383ad329efa8e505f6bef1ce03bf6c56c35e2e
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-03 16:46:20 -0600

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
+  get '/genrecount', to: 'movies#genrecount'
 
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
